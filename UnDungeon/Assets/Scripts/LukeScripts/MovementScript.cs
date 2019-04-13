314317492c22f4bc555f14b0bf0b252cc9de9281
Luke M <lukeam@uci.edu>
2019-04-13 11:22:11 -0700

    Added some attacks

--- UnDungeon/Assets/Scripts/LukeScripts/MovementScript.cs
+++ UnDungeon/Assets/Scripts/LukeScripts/MovementScript.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 public class MovementScript : MonoBehaviour
 {
     public int speed;
+    private int exp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<GunFire>().Fire(1);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-
     }
 
     private void FixedUpdate()
@@ -25,5 +25,10 @@ public class MovementScript : MonoBehaviour
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0);
         
         gameObject.GetComponent<Rigidbody2D>().AddForce(force);
+    }
+
+    public void addXP()
+    {
+
     }
 }
---
+++ UnDungeon/Assets/Scripts/LukeScripts/Shotgun.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : MonoBehaviour
+{
+    public GameObject bullet;
+    Vector2 bulletPos;
+
+    public void Fire()
+    {
+        bulletPos = transform.position;
+        Instantiate(bullet, bulletPos, transform.rotation).GetComponent<Rigidbody2D>();
+    }
+}

db2e5a694484cc989ccb521520d536cd9683401e
Luke M <lukeam@uci.edu>
2019-04-14 17:40:37 -0700

    j

--- UnDungeon/Assets/EndGame.cs
+++ UnDungeon/Assets/EndGame.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class EndGame : MonoBehaviour
 {
+    public GameObject crown;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+        {
+            
+            Instantiate(crown,transform.position,Quaternion.identity);
+        }
     }
 }

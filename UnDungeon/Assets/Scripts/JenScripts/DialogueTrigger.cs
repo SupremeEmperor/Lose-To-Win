2a5a7aab7390030268bbcdf2e0a219da0199aead
Fireryrex <victorwz@uci.edu>
2019-04-14 17:34:40 -0700

    periodic update

--- UnDungeon/Assets/Scripts/JenScripts/DialogueTrigger.cs
+++ UnDungeon/Assets/Scripts/JenScripts/DialogueTrigger.cs
@@ -6,12 +6,21 @@ public class DialogueTrigger : MonoBehaviour
 {
     //public Dialogue dialogue;
     public Conversation conversation;
+    public MovementScript player;
+    public GunFire shooting;
+
+    private void Start()
+    {
+        player = (MovementScript)GameObject.FindWithTag("Player").GetComponent(typeof(MovementScript));
+        shooting = (GunFire)GameObject.FindWithTag("Player").GetComponent(typeof(GunFire));
+    }
 
     public void TriggerDialogue()
     {
         //Time.timeScale = 0f;
+        player.setNoEnemies(true);
+        shooting.setShoot(true);
         FindObjectOfType<DialogueManager>().StartDialogue(conversation.conversation);
-        
     }
 
 }
